873aca46d479e0f318d962998658e4f51a60810d
Yarit Villalobos <yarit.villalobos@mail.escuelaing.edu.co>
2021-04-23 13:04:40 -0500

    finalizado

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -1,9 +1,34 @@
 var bigInt = require("big-integer");
+var memo=[0,1];
+
+function fibbo(numero){
+
+    if (numero < 0){
+        throw 'must be greater than 0'
+
+    }
+    else if (memo.length > numero){
+        return memo[numero];
+    }
+
+    else {
+        
+        memo.push(fibbo(numero-1));
+
+    }
+    return memo[numero];
+    
+    
+}
+
 module.exports = async function (context, req) {
+    
+    
+
     context.log('JavaScript HTTP trigger function processed a request.');
 
     let nth = req.body.nth
-    let nth_1 = bigInt.one;
+    /*let nth_1 = bigInt.one;
     let nth_2 = bigInt.zero;
     let answer = bigInt.zero;
 
@@ -20,7 +45,8 @@ module.exports = async function (context, req) {
             nth_1 = answer
         }
     }
-
+    */
+    let answer = fibbo(nth); 
     context.res = {
         body: answer.toString()
     };
